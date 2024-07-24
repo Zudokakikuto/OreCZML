60d87b2c09c2d8c70a58e30f5332a0f86908e234
Julien LEBLOND <julien.leblond@student.isae-supaero.fr>
2024-07-24 11:56:18 +0200

    Rework for the wiki on GitLab/GitHub.
    Renamed : SatellitePosition to TimePosition.
            : AbstractPointOnEarth to AbstractPointOnBody
    
    CheckStyle update

--- src/main/tutorials/TrackingVisu/SatelliteTracking.java
+++ src/main/tutorials/TrackingVisu/SatelliteTracking.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package TrackingVisu;
+package main.tutorials.TrackingVisu;
 import org.hipparchus.util.FastMath;
 import org.orekit.bodies.GeodeticPoint;
 import org.orekit.bodies.OneAxisEllipsoid;
